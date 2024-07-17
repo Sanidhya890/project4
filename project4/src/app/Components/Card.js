d91d2fef77be0c26ed72540e36751735b2e7831b
Sanidhya890 <sanidhya03.neema@gmail.com>
2024-07-17 19:12:06 +0530

    Update Card.js

--- project4/src/app/Components/Card.js
+++ project4/src/app/Components/Card.js
@@ -3,10 +3,11 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const Card = ({ employee }) => {
+  const URL="https://employee-tqa0.onrender.com/";
   const handleDelete = async () => {
     try {
       const response = await axios.delete(
-        `${window.location.origin}/api/v1/employee/delete/${employee._id}`
+        `${URL}/api/v1/employee/delete/${employee._id}`
       );
       window.location.reload();
     //   if (response.data.status) {
